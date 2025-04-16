5fb68640de22b27ebaaddca412510d114994371a
spdys <noorii549@yahoo.com>
2025-04-16 18:10:08 +0300

    Renamed controller

--- src/main/kotlin/com/coded/spring/ordering/OrderingController.kt
+++ src/main/kotlin/com/coded/spring/ordering/OrderingController.kt
@@ -1,4 +1,4 @@
-package com.example.onlineOrdering
+package com.coded.spring.ordering
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
